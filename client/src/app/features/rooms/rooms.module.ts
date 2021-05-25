c5d26df0165e540ea661c3c006130c0ba0d380eb
Ido Azulay <idoazzz@gmail.com>
2021-05-25 19:53:07 +0300

    Working on remote avatars components.

--- client/src/app/features/rooms/rooms.module.ts
+++ client/src/app/features/rooms/rooms.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RoomsUserAvatarComponent } from './components/rooms-user/rooms-user-avatar.component';
-
+import { LocalUserAvatarComponent } from './components/local-user-avatar/local-user-avatar.component';
+import { RemoteUserAvatarComponent } from './components/remote-user-avatar/remote-user-avatar.component';
+import { AvatarsHolderComponent } from './components/avatars-holder/avatars-holder.component';
+import { RoomWebsocketService } from './services/room-websocket.service';
 
 @NgModule({
+  providers: [
+    RoomWebsocketService,
+  ],
   declarations: [
-    RoomsUserAvatarComponent
+    LocalUserAvatarComponent,
+    RemoteUserAvatarComponent,
+    AvatarsHolderComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    RoomsUserAvatarComponent
+    AvatarsHolderComponent,
   ]
 })
 export class RoomsModule { }
